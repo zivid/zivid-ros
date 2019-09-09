6e8c703dc375b361d40e854e8694f730e00b87b5
Stian Pedersen <stian.pedersen@gmail.com>
2019-09-09 17:42:22 +0200

    Run CI tests for old Zivid versions
    Update current Zivid version to 1.5.0, and run build_and_test for old
    versions 1.3.0 and 1.4.0 for both melodic and kinetic.
    
    Use azure-pipelines templates to reduce code duplication and make it
    easier to specify what versions should be tested against what ROS
    distros and compilers.

--- continuous-integration/run_build_and_test_in_docker.sh
+++ continuous-integration/run_build_and_test_in_docker.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -z "$CI_TEST_ZIVID_VERSION" ]; then
+  echo "Env var CI_TEST_ZIVID_VERSION not supplied!"
+  exit 1
+fi
+
 if [ -z "$CI_TEST_OS" ]; then
   echo "Env var CI_TEST_OS not supplied!"
   exit 1
@@ -10,11 +15,15 @@ if [ -z "$CI_TEST_COMPILER" ]; then
   exit 1
 fi
 
-echo "Starting $(basename $0) with CI_TEST_OS=$CI_TEST_OS, CI_TEST_COMPILER=$CI_TEST_COMPILER"
+echo "Starting $(basename $0)"
+echo "CI_TEST_ZIVID_VERSION=$CI_TEST_ZIVID_VERSION"
+echo "CI_TEST_OS=$CI_TEST_OS"
+echo "CI_TEST_COMPILER=$CI_TEST_COMPILER"
 
 docker run \
     --volume $PWD:/host  \
     --workdir /host/continuous-integration  \
+    --env CI_TEST_ZIVID_VERSION="$CI_TEST_ZIVID_VERSION" \
     --env CI_TEST_COMPILER="$CI_TEST_COMPILER" \
     $CI_TEST_OS  \
     bash -c "./build_and_test.sh" || exit $?

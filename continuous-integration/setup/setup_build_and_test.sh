e688e3ad00107950d390e1e73a17f992e8b8dc2e
Stian Pedersen <stian.pedersen@zivid.com>
2023-08-14 16:48:01 +0200

    Switch to using modern OpenCL CPU runtime for testing
    The CI system depended on a severely outdated (Ubuntu 16.04 era) Intel
    CPU runtime for OpenCL from an archive file on Dropbox. In fact the
    classic Intel CPU runtime for OpenCL has been end-of-life for a life, in
    favor of the new OneAPI stack.
    
    This commit updates the CI system to use the OpenCL runtime from the
    OneAPI stack from Intel's official repositories. This should allow us to
    add support for newer OSs since the old package was getting too old for
    that. We also then no longer have to depend on some file from Dropbox.
    
    This commit was taken from the zivid-python repo, commit
    b1be7bce34cb609a.

--- continuous-integration/setup/setup_build_and_test.sh
+++ continuous-integration/setup/setup_build_and_test.sh
@@ -27,40 +27,16 @@ else
 fi
 
 function install_opencl_cpu_runtime {
+    # Download the key to system keyring
+    INTEL_KEY_URL=https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
+    wget -O- $INTEL_KEY_URL | gpg --dearmor | tee /usr/share/keyrings/oneapi-archive-keyring.gpg > /dev/null || exit $?
 
-    echo "Begin install of OpenCL CPU runtime"
+    # Add signed entry to apt sources and configure the APT client to use Intel repository
+    echo "deb [signed-by=/usr/share/keyrings/oneapi-archive-keyring.gpg] https://apt.repos.intel.com/oneapi all main" | tee /etc/apt/sources.list.d/oneAPI.list || exit $?
+    apt update || exit $?
 
-    echo "Install driver prerequisites"
-    apt-yes install libnuma-dev lsb-core || exit $?
-
-    TMP_DIR=$(mktemp --tmpdir --directory zivid-setup-opencl-cpu-XXXX) || exit $?
-    pushd $TMP_DIR || exit $?
-    wget -q https://www.dropbox.com/s/h0txd04aqfluglq/l_opencl_p_18.1.0.015.tgz || exit $?
-    tar -xf l_opencl_p_18.1.0.015.tgz || exit $?
-    cd l_opencl_*/ || exit $?
-
-    cat > installer_config.cfg <<EOF
-# See silent.cfg in the .tgz for description of the options.
-ACCEPT_EULA=accept
-# 'yes' below is required because the installer officially supports only Ubuntu 16.04. However, it will
-# work fine on Ubuntu 18.04 as well.
-CONTINUE_WITH_OPTIONAL_ERROR=yes
-PSET_INSTALL_DIR=/opt/intel
-CONTINUE_WITH_INSTALLDIR_OVERWRITE=yes
-COMPONENTS=DEFAULTS
-PSET_MODE=install
-INTEL_SW_IMPROVEMENT_PROGRAM_CONSENT=no
-SIGNING_ENABLED=yes
-EOF
-    echo "Running Intel OpenCL driver installer."
-    echo "Note: Installer will warn about 'Unsupported operating system' if not run on Ubuntu 16.04."
-    echo "This warning can be ignored."
-    echo
-    ./install.sh --silent installer_config.cfg || exit $?
-    popd || exit $?
-    rm -r $TMP_DIR || exit $?
-
-    echo "Done installing OpenCL CPU runtime"
+    # Install the OpenCL runtime
+    apt --assume-yes install intel-oneapi-runtime-opencl intel-oneapi-runtime-compilers || exit $?
 }
 
 install_opencl_cpu_runtime || exit $?
